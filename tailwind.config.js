b4e86f5b3d0dd33fb906e728bd2a5557ebaac57a
andhika fajri <82567600+andhikfjr@users.noreply.github.com>
2025-10-06 23:21:29 +0700

    update in index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeUp: {
+          "0%": { opacity: "0", transform: "translateY(30px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeUp: "fadeUp 1s ease-out forwards",
+        fadeIn: "fadeIn 1.5s ease-out forwards",
+      },
+      fontFamily: {
+        montserrat: ['"Montserrat Alternates"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
